81796202e93e2b85ef8e23f9593678fd492f26dc
DevOashish <ashich870@gmail.com>
2023-06-22 20:52:38 +0530

    secuirty group and instance types changes

--- server.tf
+++ server.tf
@@ -73,131 +73,132 @@ resource "aws_route53_record" "catalogue" {
 }
 
 ###############################################################################
-resource "aws_instance" "user" {
-  ami = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
 
-  tags = {
-    Name = "user"
-  }
-}
-
-resource "aws_route53_record" "user" {
-  zone_id = "Z006270827E18HEX0RQPW"
-  name    = "user-dev.devoash.tech"
-  type    = "A"
-  ttl     = 300
-  records = [aws_instance.user.private_ip]
-}
-
-#############################################################################
-
-resource "aws_instance" "mysql" {
-  ami = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "mysql"
-  }
-}
-
-resource "aws_route53_record" "mysql" {
-  zone_id = "Z006270827E18HEX0RQPW"
-  name    = "mysql-dev.devoash.tech"
-  type    = "A"
-  ttl     = 300
-  records = [aws_instance.mysql.private_ip]
-}
-
-#############################################################################
-
-resource "aws_instance" "mongodb" {
-  ami = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "mongodb"
-  }
-}
-
-resource "aws_route53_record" "mongodb" {
-  zone_id = "Z006270827E18HEX0RQPW"
-  name    = "mongodb-dev.devoash.tech"
-  type    = "A"
-  ttl     = 300
-  records = [aws_instance.mongodb.private_ip]
-}
-
-############################################################################
-
-resource "aws_instance" "rabbitmq" {
-  ami = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "rabbitmq"
-  }
-}
-
-resource "aws_route53_record" "rabbitmq" {
-  zone_id = "Z006270827E18HEX0RQPW"
-  name    = "rabbitmq-dev.devoash.tech"
-  type    = "A"
-  ttl     = 300
-  records = [aws_instance.rabbitmq.private_ip]
-}
-
-###########################################################################
-
-resource "aws_instance" "payment" {
-  ami = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "payment"
-  }
-}
-
-resource "aws_route53_record" "payment" {
-  zone_id = "Z006270827E18HEX0RQPW"
-  name    = "payment-dev.devoash.tech"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.payment.private_ip]
-}
-
-#############################################################################
-
-resource "aws_instance" "shipping" {
-  ami = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "shipping"
-  }
-}
-
-resource "aws_route53_record" "shipping" {
-  zone_id = "Z006270827E18HEX0RQPW"
-  name    = "shipping-dev.devoash.tech"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.shipping.private_ip]
-}
-###############################################################################
-
-# resource "aws_instance" "web" {
-#   ami = data.aws_ami.centos.image.id
+# resource "aws_instance" "user" {
+#   ami = data.aws_ami.centos.image_id
 #   instance_type = var.instance_type
 
 #   tags = {
-#     Name = "HelloWorld"
+#     Name = "user"
 #   }
 # }
 
-# resource "aws_instance" "web" {
-#   ami = data.aws_ami.centos.image.id
+# resource "aws_route53_record" "user" {
+#   zone_id = "Z006270827E18HEX0RQPW"
+#   name    = "user-dev.devoash.tech"
+#   type    = "A"
+#   ttl     = 300
+#   records = [aws_instance.user.private_ip]
+# }
+
+# #############################################################################
+
+# resource "aws_instance" "mysql" {
+#   ami = data.aws_ami.centos.image_id
 #   instance_type = var.instance_type
+
+#   tags = {
+#     Name = "mysql"
+#   }
+# }
+
+# resource "aws_route53_record" "mysql" {
+#   zone_id = "Z006270827E18HEX0RQPW"
+#   name    = "mysql-dev.devoash.tech"
+#   type    = "A"
+#   ttl     = 300
+#   records = [aws_instance.mysql.private_ip]
+# }
+
+# #############################################################################
+
+# resource "aws_instance" "mongodb" {
+#   ami = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+
+#   tags = {
+#     Name = "mongodb"
+#   }
+# }
+
+# resource "aws_route53_record" "mongodb" {
+#   zone_id = "Z006270827E18HEX0RQPW"
+#   name    = "mongodb-dev.devoash.tech"
+#   type    = "A"
+#   ttl     = 300
+#   records = [aws_instance.mongodb.private_ip]
+# }
+
+# ############################################################################
+
+# resource "aws_instance" "rabbitmq" {
+#   ami = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+
+#   tags = {
+#     Name = "rabbitmq"
+#   }
+# }
+
+# resource "aws_route53_record" "rabbitmq" {
+#   zone_id = "Z006270827E18HEX0RQPW"
+#   name    = "rabbitmq-dev.devoash.tech"
+#   type    = "A"
+#   ttl     = 300
+#   records = [aws_instance.rabbitmq.private_ip]
+# }
+
+# ###########################################################################
+
+# resource "aws_instance" "payment" {
+#   ami = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+
+#   tags = {
+#     Name = "payment"
+#   }
+# }
+
+# resource "aws_route53_record" "payment" {
+#   zone_id = "Z006270827E18HEX0RQPW"
+#   name    = "payment-dev.devoash.tech"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.payment.private_ip]
+# }
+
+# #############################################################################
+
+# resource "aws_instance" "shipping" {
+#   ami = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+
+#   tags = {
+#     Name = "shipping"
+#   }
+# }
+
+# resource "aws_route53_record" "shipping" {
+#   zone_id = "Z006270827E18HEX0RQPW"
+#   name    = "shipping-dev.devoash.tech"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.shipping.private_ip]
+# }
+# ###############################################################################
+
+# # resource "aws_instance" "web" {
+# #   ami = data.aws_ami.centos.image.id
+# #   instance_type = var.instance_type
+
+# #   tags = {
+# #     Name = "HelloWorld"
+# #   }
+# # }
+
+# # resource "aws_instance" "web" {
+# #   ami = data.aws_ami.centos.image.id
+# #   instance_type = var.instance_type
 
 #   tags = {
 #     Name = "HelloWorld"
