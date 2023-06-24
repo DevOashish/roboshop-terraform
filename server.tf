d9508dd08b783053abfc4e17aca5113edf60ff89
DevOashish <ashich870@gmail.com>
2023-06-24 22:42:09 +0530

    for_each changes in terraform

--- server.tf
+++ server.tf
@@ -12,41 +12,69 @@ data "aws_security_group" "allow-all" {
 }
 
 
-variable "instance_type" {
-  default = "t3.micro"
-}
-
 variable "components" {
-
-  default = [ "frontend" ] 
-  
-  # "mongodb", "cart", "catalogue", "mysql", "shipping", "payment", "rabbitmq", "user" ]
-  
+  type = map(string)
+  default = {
+    frontend = {
+      name = "frontend"
+      instance_type = "t3.micro"
+    }
+    catalogue = {
+      name = "catalogue"
+      instance_type = "t3.micro"
+  }
+  cart = {
+      name = "cart"
+      instance_type = "t3.micro"
+}
+user = {
+      name = "user"
+      instance_type = "t3.micro"
+}
+mongodb = {
+      name = "mongodb"
+      instance_type = "t3.micro"
+}
+mysql = {
+      name = "mysql"
+      instance_type = "t3.micro"
+}
+shipping = {
+      name = "shipping"
+      instance_type = "t3.micro"
+}
+payment = {
+      name = "payment"
+      instance_type = "t3.micro"
+}
+rabbitmq = {
+      name = "rabbitmq"
+      instance_type = "t3.micro"
+}
+  }
 }
 
 
-
-########################################################################
 resource "aws_instance" "instance" {
-  count = length(var.components)
+  for_each = var.components
   ami = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
+  instance_type = each.value["instance.type"]
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
 
   tags = {
-    Name = var.components[count.index]
+    Name = each.value["name"]
   }
   }
 
 
-#   resource "aws_route53_record" "" {
+# resource "aws_route53_record" "records" {
+# for_each = var.components
 #   zone_id = "Z006270827E18HEX0RQPW"
-#   name    = "${var.aws_route53_record}-dev.devoash.tech"
+#   name    = "${each.value["name"]}-dev.devoash.tech"
 #   type    = "A"
-#   ttl     = 30
-#   records = [aws_instance.${var.aws_route53_record}.private_ip]
-# }
+#   ttl     = 300
+#   records = [aws_instance.instance[each.value["name"]].private_ip]
 
 
 
