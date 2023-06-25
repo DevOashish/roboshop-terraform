9a647df9b0586a7a4cc6e648ded7f4c3e2142a95
DevOashish <ashich870@gmail.com>
2023-06-25 20:06:31 +0530

    25june

--- server.tf
+++ server.tf
@@ -15,49 +15,54 @@ data "aws_security_group" "allow-all" {
 variable "components" {
   default = {
     frontend = {
-      name = "frontend"
-      instance_type = "t3.micro"
+      name          = "frontend"
+      instance_type = "t3.small"
+    }
+    mongodb = {
+      name          = "mongodb"
+      instance_type = "t3.small"
     }
     catalogue = {
-      name = "catalogue"
+      name          = "catalogue"
       instance_type = "t3.micro"
+    }
+    redis = {
+      name          = "redis"
+      instance_type = "t3.small"
+    }
+    user = {
+      name          = "user"
+      instance_type = "t3.micro"
+    }
+    cart = {
+      name          = "cart"
+      instance_type = "t3.micro"
+    }
+    mysql = {
+      name          = "mysql"
+      instance_type = "t3.small"
+    }
+    shipping = {
+      name          = "shipping"
+      instance_type = "t3.medium"
+    }
+    rabbitmq = {
+      name          = "rabbitmq"
+      instance_type = "t3.small"
+    }
+    payment = {
+      name          = "payment"
+      instance_type = "t3.small"
+    }
   }
-  cart = {
-      name = "cart"
-      instance_type = "t3.micro"
 }
-user = {
-      name = "user"
-      instance_type = "t3.micro"
-}
-mongodb = {
-      name = "mongodb"
-      instance_type = "t3.micro"
-}
-mysql = {
-      name = "mysql"
-      instance_type = "t3.micro"
-}
-shipping = {
-      name = "shipping"
-      instance_type = "t3.micro"
-}
-payment = {
-      name = "payment"
-      instance_type = "t3.micro"
-}
-rabbitmq = {
-      name = "rabbitmq"
-      instance_type = "t3.micro"
-}
-  }
-}
+
 
 
 resource "aws_instance" "instance" {
   for_each = var.components
   ami = data.aws_ami.centos.image_id
-  instance_type = each.value["instance.type"]
+  instance_type = each.value["instance_type"]
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
 
