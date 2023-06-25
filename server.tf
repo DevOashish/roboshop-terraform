0caa5e6a25d8035251b8c796824163eac5daa601
DevOashish <ashich870@gmail.com>
2023-06-25 22:18:53 +0530

    dns

--- server.tf
+++ server.tf
@@ -1,75 +1,7 @@
-data "aws_ami" "centos" {
-  most_recent      = true
-  owners           = ["973714476881"]
-    name_regex       = "Centos-8-DevOps-Practice"
-
-
-}
-
 data "aws_security_group" "allow-all" {
   name = "allow-all"
 
 }
-
-
-variable "components" {
-  default = {
-    frontend = {
-      name          = "frontend"
-      instance_type = "t3.small"
-    }
-    mongodb = {
-      name          = "mongodb"
-      instance_type = "t3.small"
-    }
-    catalogue = {
-      name          = "catalogue"
-      instance_type = "t3.micro"
-    }
-    redis = {
-      name          = "redis"
-      instance_type = "t3.small"
-    }
-    user = {
-      name          = "user"
-      instance_type = "t3.micro"
-    }
-    cart = {
-      name          = "cart"
-      instance_type = "t3.micro"
-    }
-    mysql = {
-      name          = "mysql"
-      instance_type = "t3.small"
-    }
-    shipping = {
-      name          = "shipping"
-      instance_type = "t3.medium"
-    }
-    rabbitmq = {
-      name          = "rabbitmq"
-      instance_type = "t3.small"
-    }
-    payment = {
-      name          = "payment"
-      instance_type = "t3.small"
-    }
-  }
-}
-
-
-
-resource "aws_instance" "instance" {
-  for_each = var.components
-  ami = data.aws_ami.centos.image_id
-  instance_type = each.value["instance_type"]
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-
-
-  tags = {
-    Name = each.value["name"]
-  }
-  }
 
 
 resource "aws_route53_record" "records" {
@@ -80,3 +12,92 @@ resource "aws_route53_record" "records" {
   ttl     = 30
   records = [aws_instance.instance[each.value["name"]].private_ip]
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# data "aws_ami" "centos" {
+#   most_recent      = true
+#   owners           = ["973714476881"]
+#     name_regex       = "Centos-8-DevOps-Practice"
+
+
+# }
+
+
+# variable "components" {}
+#   default = {
+#     frontend = {
+#       name          = "frontend"
+#       instance_type = "t3.small"
+#     }
+#     mongodb = {
+#       name          = "mongodb"
+#       instance_type = "t3.small"
+#     }
+#     catalogue = {
+#       name          = "catalogue"
+#       instance_type = "t3.micro"
+#     }
+#     redis = {
+#       name          = "redis"
+#       instance_type = "t3.small"
+#     }
+#     user = {
+#       name          = "user"
+#       instance_type = "t3.micro"
+#     }
+#     cart = {
+#       name          = "cart"
+#       instance_type = "t3.micro"
+#     }
+#     mysql = {
+#       name          = "mysql"
+#       instance_type = "t3.small"
+#     }
+#     shipping = {
+#       name          = "shipping"
+#       instance_type = "t3.medium"
+#     }
+#     rabbitmq = {
+#       name          = "rabbitmq"
+#       instance_type = "t3.small"
+#     }
+#     payment = {
+#       name          = "payment"
+#       instance_type = "t3.small"
+#     }
+#   }
+# }
+
+
+
+# resource "aws_instance" "instance" {
+#   for_each = var.components
+#   ami = data.aws_ami.centos.image_id
+#   instance_type = each.value["instance_type"]
+#   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+
+
+#   tags = {
+#     Name = each.value["name"]
+#   }
+#   }
+
+
