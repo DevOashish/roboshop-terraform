47106fd776e11d496cbc0f433317eaef035fe5f2
DevOashish <ashich870@gmail.com>
2023-06-25 22:24:35 +0530

    dns

--- server.tf
+++ server.tf
@@ -4,6 +4,19 @@ data "aws_security_group" "allow-all" {
 }
 
 
+resource "aws_instance" "instance" {
+  for_each = var.components
+  ami = data.aws_ami.centos.image_id
+  instance_type = each.value["instance_type"]
+  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+
+
+  tags = {
+    Name = each.value["name"]
+  }
+  }
+
+
 resource "aws_route53_record" "records" {
   for_each               = var.components
   zone_id = "Z006270827E18HEX0RQPW"
