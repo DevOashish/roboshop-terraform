dcd45b21dc3442b22fbbc6f7be8c66374db071e7
DevOashish <ashich870@gmail.com>
2023-07-07 12:41:29 +0530

    dns

--- server.tf
+++ server.tf
@@ -1,7 +1,7 @@
 module "database-servers" {
   for_each = var.database_servers
   source = "./module"
-  components_name = each.value["name"]
+  component_name = each.value["name"]
   env = var.env
   instance_type = each.value["instance_type"]
   password = lookup(each.value, "password", "null")
@@ -15,7 +15,7 @@ module "app-servers" {
   depends_on = [module.database-servers]
   for_each = var.app_servers
   source = "./module"
-  components_name = each.value["name"]
+  component_name = each.value["name"]
   env = var.env
   instance_type = each.value["instance_type"]
   password = lookup(each.value, "password", "null")
